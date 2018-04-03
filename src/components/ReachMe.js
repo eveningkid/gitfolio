e665737e0541598068bc929c5959399a16abd8ea
Luiz Nickel <eu@luiznickel.com>
2018-04-03 16:05:51 -0300

    Show github profile shortcut if email isn't public

--- src/components/ReachMe.js
+++ src/components/ReachMe.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import './ReachMe.scss'
+import "./ReachMe.scss";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     user: state.user,
-  }
-}
+    login: state.login
+  };
+};
 
-const ReachMe = ({user}) => {
-  const {
-    email,
-    hireable,
-  } = user
+const ReachMe = ({ user }) => {
+  const { email, hireable, login } = user;
 
-  const mailtoAddress = `mailto:${email}?subject=I checked your portfolio and...`
+  const mailtoAddress = `mailto:${email}?subject=I checked your portfolio and...`;
+  const profile = `https://github.com/${login}/`;
 
   return (
     <div className="ReachMe">
-      <a href={mailtoAddress}>
-        <button>
-          why not getting in touch?
-        </button>
-      </a>
+      {email == null ? (
+        <a href={profile}>
+          <button>why not getting in touch?</button>
+        </a>
+      ) : (
+        <a href={mailtoAddress}>
+          <button>why not getting in touch?</button>
+        </a>
+      )}
 
-      <span>
-        {
-          hireable ? (
+      {email !== null ? (
+        <span>
+          {hireable ? (
             <span>
               plus, I'm now <a href={mailtoAddress}>hireable</a>
             </span>
@@ -35,11 +38,15 @@ const ReachMe = ({user}) => {
             <span>
               I could soon be <a href={mailtoAddress}>hireable</a>
             </span>
-          )
-        }
-      </span>
+          )}
+        </span>
+      ) : (
+        <span>
+          Reach me at my Github profile <a href={profile}>here</a>
+        </span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default connect(mapStateToProps)(ReachMe)
+export default connect(mapStateToProps)(ReachMe);
