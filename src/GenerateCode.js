89126b66dd1e8d1e2e347ae0dd665f0c2cec1751
eveningkid <arn.dellinger@gmail.com>
2018-04-06 22:30:40 +0800

    Added viewport meta

--- src/GenerateCode.js
+++ src/GenerateCode.js
@@ -60,6 +60,7 @@ const GenerateCode = React.createClass({
     <title>Portfolio</title>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="https://gitfolio-f2c18.firebaseapp.com/static/css/all.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </head>
 
   <body>
