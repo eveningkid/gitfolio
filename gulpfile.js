4899b9c641c0ba1783f2a0def4d2ac286aeadb8d
Welington da Silva <thebylito@gmail.com>
2018-06-07 14:15:39 -0400

    auto add css to build with hash

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var hash = require('gulp-hash')
 var concat = require('gulp-concat')
 var cleanCSS = require('gulp-clean-css')
+var rename = require('gulp-rename');
+var replace  =  require('gulp-replace');
 
+var hashedCSS;
 gulp.task('sass', function () {
   return gulp.src(
     './src/**/*.scss'
   )
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(concat('all.min.css'))
   .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(concat('all.css'))
+  .pipe(hash())
+  .pipe(rename(function (path) {
+    path.basename += "-min";
+    hashedCSS = path.basename + '.css';
+  }))
   .pipe(gulp.dest('./build/static/css'))
 })
+
+gulp.task('htmlreplace', ['sass'], function () {
+  return gulp.src('./build/index.html')
+  .pipe(replace('css.css', '/static/css/'+hashedCSS))
+  .pipe(gulp.dest('./build/'));
+});
