84d93cc83b90053fe7ad22aca8f6b17094c27a9d
thebylito <thebylito@gmail.com>
2018-06-08 00:03:01 -0400

    Update ReachMe.js

--- src/components/ReachMe.js
+++ src/components/ReachMe.js
@@ -3,8 +3,8 @@ import { connect } from "react-redux";
 
 import "./ReachMe.scss";
 
-const mapStateToProps = state => {
-  const { user, login } = state;
+const mapStateToProps = ({ user, login }) => {
+
   return {
     user, login
   };
